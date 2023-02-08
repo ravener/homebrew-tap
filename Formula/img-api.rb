8e1526eabd6931c30e65de393acc344dfc661462
Raven Ravener <ravener.anime@gmail.com>
2023-02-08 08:56:02 +0430

    Brew formula update for img-api version v1.2.5

--- Formula/img-api.rb
+++ Formula/img-api.rb
@@ -5,32 +5,52 @@
 class ImgApi < Formula
   desc "An image server api for generating memes for Discord Bots."
   homepage "https://github.com/ravener/img-api"
-  version "1.2.4"
+  version "1.2.5"
   license "MIT"
-  bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ravener/img-api/releases/download/v1.2.4/img-api_1.2.4_Darwin_x86_64.tar.gz"
-    sha256 "2bbc01bdcf60b82c4fad8a9079ac8d405cfddbeead106d8d9a9fe8c5e09ccd25"
-  end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ravener/img-api/releases/download/v1.2.4/img-api_1.2.4_Darwin_arm64.tar.gz"
-    sha256 "1ea91af15c8b48dbbbb4fb2621bedf63ccbbc3ec1008119bc8ce7329eba73c98"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ravener/img-api/releases/download/v1.2.4/img-api_1.2.4_Linux_x86_64.tar.gz"
-    sha256 "4cf4c87b64614db2c1a3e3eec3007ecdab12d19343df010ac07846e92ef95ae9"
-  end
-  if OS.linux? && Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-    url "https://github.com/ravener/img-api/releases/download/v1.2.4/img-api_1.2.4_Linux_armv6.tar.gz"
-    sha256 "6b87a364b20e3f83bef5a9b849ced5a32cb0920eb0c215d9001337222d79a839"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ravener/img-api/releases/download/v1.2.4/img-api_1.2.4_Linux_arm64.tar.gz"
-    sha256 "01751cbe6b71055278bbee6931e4addc29e24b622f436ad7e1706bb0fe2e3b31"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ravener/img-api/releases/download/v1.2.5/img-api_1.2.5_Darwin_arm64.tar.gz"
+      sha256 "6689208971c31dbf0d4ae962ba8068c713dd88d2c20328c7874509a946447431"
+
+      def install
+        bin.install_symlink "img-api"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ravener/img-api/releases/download/v1.2.5/img-api_1.2.5_Darwin_x86_64.tar.gz"
+      sha256 "10ac56471463cff453b071f0e3d1d75d61b70476cfce86e054e7dcc9ec932286"
+
+      def install
+        bin.install_symlink "img-api"
+      end
+    end
   end
 
-  def install
-    bin.install_symlink "img-api"
+  on_linux do
+    if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
+      url "https://github.com/ravener/img-api/releases/download/v1.2.5/img-api_1.2.5_Linux_armv6.tar.gz"
+      sha256 "85197230d4b5e482620598c57c985d76e9ec92a16159dd462df76675dec40371"
+
+      def install
+        bin.install_symlink "img-api"
+      end
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ravener/img-api/releases/download/v1.2.5/img-api_1.2.5_Linux_arm64.tar.gz"
+      sha256 "44d1841f91dd697616c3678303f394d3f2dafd926c376b955cecbfbdeb6c767f"
+
+      def install
+        bin.install_symlink "img-api"
+      end
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ravener/img-api/releases/download/v1.2.5/img-api_1.2.5_Linux_x86_64.tar.gz"
+      sha256 "2a2a79c8288c2ebf21e8205cc5e4477992fdf5a3df46571ca225174731751cf8"
+
+      def install
+        bin.install_symlink "img-api"
+      end
+    end
   end
 end
